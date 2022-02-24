ece05bd784053a27cef59a2e602d91d84890e477
unknown <omura.takayuki@gmail.com>
2022-02-24 15:28:39 +0900

    0224FizzBuzz・Pythagoras課題レビュー用_再

--- Pythagoras/Pythagoras.rb
+++ Pythagoras/Pythagoras.rb
@@ -1,10 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
-#sinatoraのURL(http://localhost:4567/)アクセス時に:pythagorascd.erbを実行する処理
+#sinatoraのURL(http://localhost:4567/)アクセス時に:を実行する処理
 get "/" do
   erb :pythagoras
 end
-
+#pythagorascd.erbの入力値を受け取り斜辺cを計算する処理
 get "/calcurate" do
   @edge_a = params[:edge_a].to_f
   @edge_b = params[:edge_b].to_f
