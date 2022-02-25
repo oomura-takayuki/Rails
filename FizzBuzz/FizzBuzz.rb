9bafde3e630d0fcf3490f39ec2b5a0e4edb76dd3
unknown <omura.takayuki@gmail.com>
2022-02-25 14:28:25 +0900

    0225レビュー指摘対応、FizzBuzz：出力結果修正、Pythagoras：erbファイル切り分け

--- FizzBuzz/FizzBuzz.rb
+++ FizzBuzz/FizzBuzz.rb
@@ -1,13 +1,13 @@
-for count in 1..100 do
-    num1 = count % 3;
-    num2 = count % 5;
-    if num1 == 0 && num2 == 0  then
-        puts "15の倍数"
-    elsif num1 == 0 then
-        puts "3の倍数"
-    elsif num2 == 0 then
-        puts "5の倍数"    
-    else
-        p count   
-    end
+for count in 1..100
+  num1 = count % 3
+  num2 = count % 5
+  if num1 == 0 && num2 == 0
+    puts "FizzBuzz"
+  elsif num1 == 0
+    puts "Fizz"
+  elsif num2 == 0
+    puts "Buzz"
+  else
+    p count
+  end
 end
